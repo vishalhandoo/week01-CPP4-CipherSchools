16aa214a12c0402cd8a82da81f0a4449b908046a
vishalhandoo <vishalhandoo067@gmail.com>
2023-01-21 10:48:56 +0530

    For Verification

---
+++ Day-0/cal.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int sum(int n1,int n2){
+    return n1+n2;
+}
+
+int main(){
+    int n1,n2;
+    cin>>n1>>n2;
+    cout<<"The sum of n1 & n2 is : "<<sum(n1,n2);
+}
